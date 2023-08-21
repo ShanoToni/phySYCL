44445fcf0c035eb3905d43a307fc29a1acd67846
ShanoToni <16207327+ShanoToni@users.noreply.github.com>
2023-08-21 21:01:01 +0100

    Point Interesection to 2D geom added

--- include/geometry2D.hpp
+++ include/geometry2D.hpp
@@ -16,7 +16,7 @@ typedef struct Line2D {
 
 float length(const Line2D &line);
 
-float lengthSq(const Line2D &line);
+float length_sq(const Line2D &line);
 
 // Circle 2D
 typedef struct Circle {
@@ -25,5 +25,41 @@ typedef struct Circle {
   inline Circle() : radius(1.0f) {}
   inline Circle(const Point2D &p, float r) : position(p), radius(r) {}
 };
+
+// Rectangle
+typedef struct Rectangle2D {
+  Point2D origin;
+  vec2 size;
+
+  Rectangle2D() : size({1, 1}) {}
+  Rectangle2D(const Point2D &o, const vec2 &s) : origin(o), size(s) {}
+} Rectangle2D;
+
+vec2 get_min(const Rectangle2D &rec);
+vec2 get_max(const Rectangle2D &rec);
+
+Rectangle2D from_min_max(const vec2 &min, const vec2 &max);
+
+// Oriented rectangle
+typedef struct OrientedRectangle {
+  Point2D position;
+  vec2 halfExtents;
+  float rotation;
+
+  OrientedRectangle() : halfExtents({1, 1}), rotation(0.0f) {}
+
+  OrientedRectangle(const Point2D &p, const vec2 &h_e)
+      : position(p), halfExtents(h_e) {}
+
+  OrientedRectangle(const Point2D &p, const vec2 &h_e, float rot)
+      : position(p), halfExtents(h_e), rotation(rot) {}
+} OrientedRectangle;
+
+// Intersections
+bool point_on_line(const Point2D &p, const Line2D &line);
+bool point_in_circle(const Point2D &p, const Circle &c);
+bool point_in_rectangle(const Point2D &p, const Rectangle2D &rec);
+bool point_in_oriented_rectangle(const Point2D &p,
+                                 const OrientedRectangle &or_rec);
 
 #endif
--- src/geometry2D.cpp
+++ src/geometry2D.cpp
@@ -8,6 +8,59 @@
 
 float length(const Line2D &line) { return magnitude(line.end - line.start); }
 
-float lengthSq(const Line2D &line) {
+float length_sq(const Line2D &line) {
   return magnitude_sq(line.end - line.start);
+}
+
+vec2 get_min(const Rectangle2D &rec) {
+  vec2 p1 = rec.origin;
+  vec2 p2 = rec.origin + rec.size;
+
+  return vec2{fminf(p1.x, p2.x), fminf(p1.y, p2.y)};
+}
+
+vec2 get_max(const Rectangle2D &rec) {
+  vec2 p1 = rec.origin;
+  vec2 p2 = rec.origin + rec.size;
+
+  return vec2{fmaxf(p1.x, p2.x), fmaxf(p1.y, p2.y)};
+}
+
+Rectangle2D from_min_max(const vec2 &min, const vec2 &max) {
+  return Rectangle2D{min, max - min};
+}
+
+bool point_on_line(const Point2D &p, const Line2D &line) {
+  float dy = (line.end.y - line.start.y);
+  float dx = (line.end.x - line.start.x);
+  float M = dy / dx;
+  float B = line.start.y - M * line.start.x;
+  return CMP(p.y, M * p.x + B);
+}
+
+bool point_in_circle(const Point2D &p, const Circle &c) {
+  Line2D line(p, c.position);
+  if (length_sq(line) < c.radius * c.radius) {
+    return true;
+  }
+  return false;
+}
+
+bool point_in_rectangle(const Point2D &p, const Rectangle2D &rec) {
+  vec2 min = get_min(rec);
+  vec2 max = get_max(rec);
+
+  return min.x <= p.x && min.y <= p.y && p.x <= max.x && p.y <= max.y;
+}
+
+bool point_in_oriented_rectangle(const Point2D &p,
+                                 const OrientedRectangle &or_rec) {
+  vec2 rotVec = p - or_rec.position;
+  float theta = -DEG2RAD(or_rec.rotation);
+  float zRotation2x2[] = {cosf(theta), sinf(theta), //
+                          -sinf(theta), cosf(theta)};
+
+  Rectangle2D localRectangle(Point2D(), or_rec.halfExtents * 2.0f);
+  vec2 localPoint = rotVec + or_rec.halfExtents;
+  return point_in_rectangle(localPoint, localRectangle);
 }
